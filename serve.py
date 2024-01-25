31dfd014ec991c8671440e7e42e3957e30630fb2
MinjiaZhu <55096054+MinjiaZhu@users.noreply.github.com>
2024-01-25 10:24:46 -0500

    Update serve.py

--- serve.py
+++ serve.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def trigger_stress():
     # Start a new process to run stress_cpu.py
-    subprocess.Popen(["python", "stress_cpu.py"])
+    subprocess.Popen(["python3", "stress_cpu.py"])
     return "CPU stress process started", 200
 
 @app.route('/', methods=['GET'])
